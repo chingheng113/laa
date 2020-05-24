099501fcde1e9c1d0257dba40827863e973fcd1c
chingheng113 <chingheng113@gmail.com>
2020-05-24 14:25:39 -0400

    bk

--- experiment/binary.py
+++ experiment/binary.py
@@ -9,35 +9,27 @@ from sklearn.svm import SVC
 from sklearn.metrics import auc, roc_curve
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from imblearn import over_sampling
 from sklearn.model_selection import KFold
-import xgboost as xgb
 
 data = pd.read_csv(os.path.join('..', 'data', 'LAA_computation_lcms1_cli_class_s.csv'))
 data = data.replace({'.':np.nan, '#N/A':np.nan})
 data = data.iloc[:, 3:202]
-# data = data.dropna(subset=['Label','miR21ΔΔct ', 'H1'], axis=0)
-how_many_null = data.isnull().sum().sort_values(ascending = False)
+data = data.dropna(subset=['Label'], axis=0)
+# data = data.dropna(axis=0)
+
 Y_data = data[['Label', 'C1', 'C2', 'C3', 'C4', 'S1', 'S2', 'S3', 'S4']]
-
-label = 'C2'
-y_data = Y_data[[label]]
-
-# label = 'S2'
-# y_data = pd.cut(Y_data['S2'], [-1, 0, 9, 999], labels=[0, 1, 2]) #9
-# y_data = pd.DataFrame(y_data, columns=[label])
-# a = pd.concat((y_data, Y_data[['C2']]), axis=1)
-# a = pd.concat((a, Y_data[['S2']]), axis=1)
+y_data = Y_data[['Label']]
 
 X_data = data.drop(['Label', 'C1', 'C2', 'C3', 'C4', 'S1', 'S2', 'S3', 'S4'], axis=1)
-X_data = pd.get_dummies(X_data, columns=['sex'])
+dummy_cols = ['sex']
+X_data = pd.get_dummies(X_data, columns=dummy_cols)
 
-all_acc = []
+
+all_auroc = []
 # for i in range(10):
 #     X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.1, random_state=i, stratify=y_data)
 for train_index, test_index in KFold(n_splits=10, random_state=42, shuffle=True).split(X_data):
-    # print(len(train_index), len(test_index))
     X_train, X_test = X_data.iloc[train_index], X_data.iloc[test_index]
     y_train, y_test = y_data.iloc[train_index], y_data.iloc[test_index]
     # imputation
@@ -47,32 +39,26 @@ for train_index, test_index in KFold(n_splits=10, random_state=42, shuffle=True)
     X_test = imp.transform(X_test)
 
     # scaling
-    # scaler = preprocessing.StandardScaler()
-    scaler = preprocessing.MinMaxScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test= scaler.transform(X_test)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    X_train = min_max_scaler.fit_transform(X_train)
+    X_test = min_max_scaler.transform(X_test)
 
     # over-sampling
-    # print('before', y_train.groupby([label]).size())
+    # print('before', y_train.groupby(['Label']).size())
     sm = over_sampling.SVMSMOTE(random_state=42)
     X_train, y_train = sm.fit_resample(X_train, y_train)
-    # print('after', y_train.groupby([label]).size())
+    # print('after', y_train.groupby(['Label']).size())
 
     # define the model
-    # model = ExtraTreesClassifier(n_estimators=250,  random_state=0)
-    # model = SVC(kernel='linear', gamma='auto', probability=True, class_weight='balanced')
-    model = xgb.XGBClassifier(objective='multi:softmax', learning_rate=0.5)
+    model = ExtraTreesClassifier(n_estimators=250,  random_state=42)
+    # model = SVC(kernel='linear', probability=True)
     model.fit(X_train, y_train.values.ravel())
-    y_pred = model.predict(X_test)
-    # print(classification_report(y_test, y_pred))
-    # print(confusion_matrix(y_test, y_pred))
-
-    # true_labels = y_test.apply(np.argmax, axis=1)
-    # pred_labels = np.argmax(y_pred, axis=1)
-    acc = accuracy_score(y_test, y_pred)
-    print(acc)
-    all_acc.append(acc)
-print(np.mean(all_acc), np.std(all_acc))
+    y_pred = model.predict_proba(X_test)
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred[:, 1])
+    auroc = auc(fpr, tpr)
+    print('auc', auroc)
+    all_auroc.append(auroc)
+print(np.mean(all_auroc), np.std(all_auroc))
 print('done')
 
 
---
+++ experiment/dbscan.py
@@ -0,0 +1,84 @@
+import numpy as np
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.impute import SimpleImputer
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+import pandas as pd
+import os
+# #############################################################################
+data = pd.read_csv(os.path.join('..', 'data', 'LAA_computation_lcms1_cli_class_s.csv'))
+data = data.replace({'.':np.nan, '#N/A':np.nan})
+data = data.iloc[:, 3:202]
+# data = data.dropna(subset=['Label','miR21ΔΔct ', 'H1'], axis=0)
+how_many_null = data.isnull().sum().sort_values(ascending = False)
+Y_data = data[['Label', 'C1', 'C2', 'C3', 'C4', 'S1', 'S2', 'S3', 'S4']]
+label = 'C2'
+y_data = Y_data[[label]].values.ravel()
+
+X_data = data.drop(['Label', 'C1', 'C2', 'C3', 'C4', 'S1', 'S2', 'S3', 'S4'], axis=1)
+X_data = pd.get_dummies(X_data, columns=['sex'])
+
+
+# imputation or drop na
+# X_data = X_data.dropna(axis=0)
+imp = SimpleImputer(missing_values=np.nan, strategy='median')
+X_data = imp.fit_transform(X_data)
+
+# scaling
+# scaler = preprocessing.MinMaxScaler()
+scaler = preprocessing.StandardScaler()
+X_data = scaler.fit_transform(X_data)
+
+# Dimension reduction
+X_data = PCA(n_components=2).fit_transform(X_data)
+
+
+# #############################################################################
+# Compute DBSCAN
+db = DBSCAN(eps=1.5, min_samples=5).fit(X_data)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+
+print('Estimated number of clusters: %d' % n_clusters_)
+print('Estimated number of noise points: %d' % n_noise_)
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(y_data, labels))
+print("Completeness: %0.3f" % metrics.completeness_score(y_data, labels))
+print("V-measure: %0.3f" % metrics.v_measure_score(y_data, labels))
+print("Adjusted Rand Index: %0.3f"
+      % metrics.adjusted_rand_score(y_data, labels))
+print("Adjusted Mutual Information: %0.3f"
+      % metrics.adjusted_mutual_info_score(y_data, labels))
+print("Silhouette Coefficient: %0.3f"
+      % metrics.silhouette_score(X_data, labels))
+
+# #############################################################################
+# Plot result
+import matplotlib.pyplot as plt
+
+# Black removed and is used for noise instead.
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
+
+    class_member_mask = (labels == k)
+
+    xy = X_data[class_member_mask & core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=14)
+
+    xy = X_data[class_member_mask & ~core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=6)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
